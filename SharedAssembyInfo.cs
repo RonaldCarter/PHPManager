1e7c22cff42461a0048a4ef898c394ab82a2a968
Ronald Carter <RonaldCarter@users.noreply.github.com>
2017-07-13 10:05:22 +1000

    Another attempt t upgrading to 4.6.2 dotNET

--- SharedAssembyInfo.cs
+++ SharedAssembyInfo.cs
@@ -1,20 +1,11 @@
-﻿//-----------------------------------------------------------------------
-// <copyright>
-// Copyright (C) Ruslan Yakushev for the PHP Manager for IIS project.
-//
-// This file is subject to the terms and conditions of the Microsoft Public License (MS-PL).
-// See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL for more details.
-// </copyright>
-//----------------------------------------------------------------------- 
-
-using System.Reflection;
+﻿using System.Reflection;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyCompany("PHP Manager Project")]
 [assembly: AssemblyProduct("PHP Manager for IIS")]
-[assembly: AssemblyCopyright("Copyright © PHP Manager Project 2011")]
+[assembly: AssemblyCopyright("Copyright © Ronald Carter 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,5 +19,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
